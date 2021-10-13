0e7d7bd5d359e2b55e56762e5049ffbd19f1b209
Jorel martins deczuta <81186935+Jojorel@users.noreply.github.com>
2021-10-13 11:19:37 -0300

    vetor1

---
+++ vetor1.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class vetor1 {
+    public static void main(String args[]){
+        int n[]= {3,2,8,7,5,4};
+        Arrays.sort(n); // coloca em ordem
+        for(int valor:n){
+            System.out.println(valor+ " ");
+        }
+
+
+    }
+}
